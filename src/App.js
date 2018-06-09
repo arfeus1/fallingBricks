216826f351224248ec4dff04a41393570dcc135a
Igor Rykhlevskyi <rykhlevskyi@gmail.com>
2018-06-10 01:37:34 +0200

    Adding canvas components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import Canvas from './components/Canvas';
 
 class App extends Component {
-  	render() {
-    	return (
-      		<div className="App">
-				<h1>Awesome bircks</h1>
-      		</div>
-    	);
-  	}
+  render() {
+    return (
+      <Canvas />
+    );
+  }
 }
 
 export default App;
